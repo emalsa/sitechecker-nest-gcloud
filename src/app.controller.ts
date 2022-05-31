33818874b2b97aa249e6b14e276a74f3fe62cd4a
dni <daniele@nicastro.io>
2022-06-01 00:14:59 +0200

    v3with goth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,32 @@
 import {Controller, Get} from '@nestjs/common';
 import {AppService} from './app.service';
 // import {stringify} from "flatted";
-
 // const {http, https} = require('follow-redirects');
+import got from 'got';
+
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {
     }
 
+    @Get('/time')
+    async getTime() {
+        const data = await got.get('https://badssl.com', {}).then((res) => {
+            return res.timings;
+        });
+
+        console.log(data);
+        return data
+    }
+
     @Get('/test')
     async getTest() {
-        const url = require('url');
-        const {http, https} = require('follow-redirects');
 
-        const options = url.parse('http://tinyurl.com/2p95z4n4');
-        console.log(options)
+
+        const url = require('url');
+        const {https} = require('follow-redirects');
+        const options = url.parse('https://nicastro.io');
         options.maxRedirects = 10;
         options.trackRedirects = (options, response, request) => {
             // Use this to adjust the request options upon redirecting,
@@ -34,16 +45,30 @@ export class AppController {
             // console.log(response)
         };
 
-        const request = http.request(options, response => {
+        const request = https.request(options, response => {
             console.log(response.responseUrl);
             console.log(response.socket.authorized || false)
             console.log(response.socket.authorizationError)
             console.log(response.redirects)
 
-            console.log( response.elapsedTime);
+            // console.log('Time elapsed:', new Date().getTime() - start_time);
+            // console.log(response);
             // 'http://duckduckgo.com/robots.txt'
+            let data = '';
+            let start1 = process.hrtime();
+            response.on('data', (chunk) => {
+                data = data + chunk.toString();
+                console.log("datza");
+            });
 
+            response.on('end', () => {
+                // const body = JSON.parse(data);
+                // console.log(body);
+                let elapsed1 = process.hrtime(start1)[1] / 1000000;
+                console.log("Http elapsed time:", elapsed1);
+            });
         });
+
 
         request.end();
     }
